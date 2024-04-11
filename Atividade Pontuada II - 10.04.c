27cbed0b41142e4a6857302cd35083ae87d1b9e0
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2024-04-11 08:13:07 -0300

    Add files via upload

--- Atividade Pontuada II - 10.04.c
+++ Atividade Pontuada II - 10.04.c
@@ -2,23 +2,26 @@
 #include <locale.h>
 #include <stdlib.h>
 
+#define DISCIPLINAS 3
+#define NOTAS 3
+
 int main() {
 	setlocale(LC_ALL,"");
-    char disciplinas[3][50];
-    float notas[3][3];
+    char disciplinas[DISCIPLINAS][50];
+    float notas[DISCIPLINAS][NOTAS];
     float pesos[3] = {3, 3, 4};
-    float media[3], somaNotas = 0;
+    float media[DISCIPLINAS], somaNotas = 0;
     int i, j;
 
   
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < DISCIPLINAS; i++) {
         printf("Nome da %iª disciplina: ", i+1);
         scanf("%s", disciplinas[i]);
     }
 
   
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    for (i = 0; i < DISCIPLINAS; i++) {
+        for (j = 0; j < NOTAS; j++) {
             do {
                 printf("%dª nota da disciplina %s: ", j+1, disciplinas[i]);
                 scanf("%f", &notas[i][j]);
@@ -30,8 +33,8 @@ int main() {
     }
 
   
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    for (i = 0; i < DISCIPLINAS; i++) {
+        for (j = 0; j < NOTAS; j++) {
             somaNotas += notas[i][j] * pesos[j];
         }
         media[i] = somaNotas / 10;
@@ -40,11 +43,11 @@ int main() {
 
    system("cls || clear");
    
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < DISCIPLINAS; i++) {
         printf("\nDisciplina: %s\n", disciplinas[i]);
         printf("Média: %.1f\n", media[i]);
         
-        for(j = 0; j < 3; j++){
+        for(j = 0; j < NOTAS; j++){
         	printf("%iª nota: %.1f\n", j+1, notas[i][j]);
 		}
     }
